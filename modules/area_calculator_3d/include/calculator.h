644bd450c6e4d3bcf332fdffafc19066485f73f0
Epremyan Norik <epnorik@yandex.ru>
2020-06-06 16:02:46 +0300

    Update calculator.h

--- modules/area_calculator_3d/include/calculator.h
+++ modules/area_calculator_3d/include/calculator.h
@@ -47,6 +47,6 @@ class cylinder : public figure {
 // Factories
 class Factory {
  public:
-    static figure* Factory::create(std::string name);
+    figure* Factory::create(std::string name);
 };
 #endif  // MODULES_AREA_CALCULATOR_3D_INCLUDE_CALCULATOR_H_
